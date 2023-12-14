693047680d6c520755e0acbd75aad64b1899f7c6
Rasul Ismailov <beeline09@gmail.com>
2023-12-14 12:25:10 +0300

    Добавил календарь

--- composeApp/src/androidMain/kotlin/ru/weatherclock/adg/App.android.kt
+++ composeApp/src/androidMain/kotlin/ru/weatherclock/adg/App.android.kt
@@ -3,7 +3,12 @@ package ru.weatherclock.adg
 import android.app.Application
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import ru.weatherclock.adg.app.domain.di.initKoin
@@ -28,9 +33,26 @@ class AndroidApp: Application() {
 class AppActivity: ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState)
         setContent {
             App()
+        }
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val controller = window.insetsController
+
+            if (controller != null) {
+                controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                controller.systemBarsBehavior =
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
         }
     }
 }
--- composeApp/src/commonMain/kotlin/ru/weatherclock/adg/app/presentation/components/calendar/Calendar.kt
+++ composeApp/src/commonMain/kotlin/ru/weatherclock/adg/app/presentation/components/calendar/Calendar.kt
@@ -21,14 +21,19 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.AwaitPointerEventScope
+import androidx.compose.ui.input.pointer.PointerEvent
+import androidx.compose.ui.input.pointer.PointerEventPass
 import androidx.compose.ui.input.pointer.PointerEventType
-import androidx.compose.ui.input.pointer.onPointerEvent
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ru.weatherclock.adg.app.presentation.components.calendar.dateTypes.DateInput
@@ -49,134 +54,107 @@ fun Calendar(
     background: Color,
     locale: DateInputDefaults.DateInputLocale,
 ) {
-    val windowState = remember { mutableStateOf(CalendarWindowState.CALENDAR) }
-    val textSize = remember { mutableStateOf(50.sp.value) }
-    when (windowState.value) {
-        CalendarWindowState.CALENDAR -> {
-//            Row(
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .height(32.dp),
-//                horizontalArrangement = Arrangement.Center,
-//                verticalAlignment = Alignment.CenterVertically
-//            ) {
-//                CalendarHeader(
-//                    dateTime,
-//                    locale
-//                )
-//            }
-            val dates = dateTime.value.getMonthGrid()
-            Column(
+    val dates = dateTime.value.getMonthGrid()
+    Column(
+        Modifier
+            .wrapContentHeight(align = Alignment.CenterVertically)
+            .wrapContentWidth(align = Alignment.CenterHorizontally)
+            .aspectRatio(1f)
+            .background(Color.Gray)
+    ) {
+        weekHeader()
+        Spacer(Modifier.height(5.dp))
+        dates.filter { it.isNotEmpty() }.forEach { week ->
+            Row(
                 Modifier
-                    .wrapContentHeight(align = Alignment.CenterVertically)
-                    .wrapContentWidth(align = Alignment.CenterHorizontally)
-                    .aspectRatio(1f)
-                    .background(Color.Gray)
+                    .fillMaxWidth()
             ) {
-                weekHeader()
-                Spacer(Modifier.height(5.dp))
-                dates.filter { it.isNotEmpty() }.forEach { week ->
-                    Row(
-                        Modifier
-                            .fillMaxWidth()
-                    ) {
-                        week.forEach { date ->
-                            var hovered by remember { mutableStateOf(false) }
-                            val selected by derivedStateOf {
-                                date?.let {
-                                    dateHolder.value.checkIfSelected(
-                                        date,
-                                        dateTime
-                                    )
-                                } ?: false
-                            }
-                            AutoSizeText(
-                                text = (date?.toString() ?: ""),
-                                minTextSize = 5.sp,
-                                maxTextSize = 50.sp,
-                                stepGranularityTextSize = 5.sp,
-                                maxLines = 1,
-                                style = MaterialTheme.typography.bodySmall,
-//                                fontSize = 12.sp,
-                                color = if (selected) Color.Black else Color.White,
-                                modifier = Modifier
-                                    .also {
-                                        if (date != null) {
-                                            it.getPointerCursor()
-                                        }
-                                    }
-                                    .weight(1f)
-                                    .aspectRatio(1f)
-//                                    .fillMaxSize()
-                                    .clip(RoundedCornerShape(percent = 50))
-                                    .border(
-                                        1.dp,
-                                        if (hovered) Color.White else Color.Transparent,
-                                        RoundedCornerShape(percent = 50)
-                                    )
-//                                    .background(Color.Blue)
-                                    .onPointerEvent(eventType = PointerEventType.Enter) {
-                                        date?.let {
-                                            hovered = true
-                                        }
-                                    }
-                                    .onPointerEvent(eventType = PointerEventType.Exit) {
-                                        date?.let {
-                                            hovered = false
-                                        }
-                                    }
-                                    .clickable(
-                                        enabled = date != null
-                                    ) {
-                                        dateHolder.value.select(
-                                            date!!,
-                                            dateTime,
-                                            windowState
-                                        )
-                                    }
-                                    .background(if (selected) Color.White else Color.Transparent)
-                                    .padding(vertical = 2.dp),
-                                alignment = Alignment.Center
-//                                textAlign = TextAlign.Center
+                week.forEach { date ->
+                    var hovered by remember { mutableStateOf(false) }
+                    val selected by derivedStateOf {
+                        date?.let {
+                            dateHolder.value.checkIfSelected(
+                                date,
+                                dateTime
                             )
-                        }
+                        } ?: false
                     }
+                    AutoSizeText(
+                        text = (date?.toString() ?: ""),
+                        minTextSize = 5.sp,
+                        maxTextSize = 50.sp,
+                        stepGranularityTextSize = 5.sp,
+                        maxLines = 1,
+                        style = MaterialTheme.typography.bodySmall,
+//                                fontSize = 12.sp,
+                        color = if (selected) Color.Black else Color.White,
+                        modifier = Modifier
+                            .also {
+                                if (date != null) {
+                                    it.getPointerCursor()
+                                }
+                            }
+                            .weight(1f)
+                            .aspectRatio(1f)
+//                                    .fillMaxSize()
+                            .clip(RoundedCornerShape(percent = 50))
+                            .border(
+                                1.dp,
+                                if (hovered) Color.White else Color.Transparent,
+                                RoundedCornerShape(percent = 50)
+                            )
+//                                    .background(Color.Blue)
+                            .onPointerEvent(eventType = PointerEventType.Enter) {
+                                date?.let {
+                                    hovered = true
+                                }
+                            }
+                            .onPointerEvent(eventType = PointerEventType.Exit) {
+                                date?.let {
+                                    hovered = false
+                                }
+                            }
+                            .clickable(
+                                enabled = date != null
+                            ) {
+                                dateHolder.value.select(
+                                    date!!,
+                                    dateTime,
+                                    mutableStateOf(CalendarWindowState.CALENDAR)
+                                )
+                            }
+                            .background(if (selected) Color.White else Color.Transparent)
+                            .padding(vertical = 2.dp),
+                        alignment = Alignment.Center
+//                                textAlign = TextAlign.Center
+                    )
                 }
             }
         }
+    }
+}
 
-        CalendarWindowState.TIME -> {
-            TimeInput(
-                dateHolder = dateHolder,
-//                groundFieldFocusRequester = groundFieldFocusRequester,
-//                popupOpened = popupOpened,
-                windowState = windowState,
-                errorMessage = errorMessage,
-                locale = locale,
-                onDateSelected = onDateSelected,
-                background = background
-            )
+enum class CalendarWindowState {
+    CALENDAR,
+    TIME
+}
+
+@ExperimentalComposeUiApi
+fun Modifier.onPointerEvent(
+    eventType: PointerEventType,
+    pass: PointerEventPass = PointerEventPass.Main,
+    onEvent: AwaitPointerEventScope.(event: PointerEvent) -> Unit
+): Modifier = composed {
+    val currentEventType by rememberUpdatedState(eventType)
+    val currentOnEvent by rememberUpdatedState(onEvent)
+    pointerInput(pass) {
+        awaitPointerEventScope {
+            while (true) {
+                val event = awaitPointerEvent(pass)
+                if (event.type == currentEventType) {
+                    currentOnEvent(event)
+                }
+            }
         }
     }
-
-    /*if (dateHolder.value !is DateInput.SingleDateTime) {
-        Button(
-            onClick = {
-                onDateSelected(dateHolder.value.getResult())
-//                groundFieldFocusRequester.requestFocus()
-                errorMessage.value = null
-//                popupOpened.value = false
-            },
-            colors = ButtonDefaults.buttonColors(backgroundColor = background),
-            modifier = Modifier
-                .fillMaxWidth()
-                .getPointerCursor()
-        ) {
-            Text(
-                getApplyText(locale),
-                color = Color.White,
-                modifier = Modifier
-            )
-        }
-    }*/
 }
